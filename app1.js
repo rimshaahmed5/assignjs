2e70ead1ec8b6e7c64fc16a689deda318b9db99b
rimshaahmed5 <65035489+rimshaahmed5@users.noreply.github.com>
2020-06-23 23:54:24 +0500

    ready to commit

---
+++ app1.js
@@ -0,0 +1,121 @@
+//chapter no 21-25 task no 2
+//var a = prompt('Enter your favorite mobile model name');
+//var text = 'a'
+//var indxNum = text.indexOf(a);
+//var firstText = text.slice(0,indxNum)
+//document.write('my favorite model model name:'+indxNum)
+
+//chapter no 21-25 task no 3
+//var text = prompt('Enter pakistani');
+//var text = 'Pakistani'
+//var indxNum = text.indexOf('n');
+//var firstText = text.slice(0,indxNum)
+//document.write('index of n:'+indxNum)
+//
+///chapter no 21-25 task no 4
+//var text = prompt('Enter Hello World');
+//var text = 'Hello World'
+//var indxNum = text.lastIndexOf('l');
+//var firstText = text.slice(0,indxNum)
+//document.write('Last index of l:'+indxNum)
+
+//chapter no 21-25 task no 5
+//var text = prompt('Enter pakistani');
+//var text = 'Pakistani'
+//var indxNum = text.indexOf('i');
+//var firstText = text.slice(0,indxNum)
+//document.write('index of i:'+indxNum)
+//
+//chapter no 21-25 task no 7
+//var city = prompt("Enter hyderabad");
+//var a = 'Hyderabad';
+//var b = a.replace('Hyderabad','Islamabad');
+//document.write(b)
+
+//chapter no 21-25 task no 8
+//var a = 'Ali and Sami are best friends. They play cricket and football together';
+//var b = a.replace(/and/g,'&');
+//document.write(b)
+
+//chapter no 21-25 task no 9
+//var value = '427'
+//var vaa=parseInt(value);
+//document.write("Value:" + value + "<br>");
+//document.write("Type: String <br>");
+//document.write("Value:" + vaa + "<br>")
+//document.write("Type: number " )
+
+//chapter no 21-25 task no 10
+//var a = prompt('Enter peanuts')
+//var name = "peanuts";
+//name = name.toUpperCase();
+//document.write(name)
+
+//chapter no 21-25 task no 1
+//var b = prompt('Enter User input javascript')
+//var firstchart = b.slice(0,1);
+//firstchart = firstchart.toUpperCase()
+//var otherchart = b.slice(1);
+//otherchart = otherchart.toLowerCase()
+//var b = firstchart+otherchart;
+//document.write('Title case:' + " " + b)
+
+//chapter no 21-25 task no 12
+//var num = 35.36;
+//var b = num.toString();
+//document.write("Number: " + num + "<br>")
+//document.write("Result: " + b + "<br>")
+
+//chapter no 21-25 task no 13
+//var userName =prompt('Enter your user name');
+//var name = userName.length;
+//for (i=0; i < userName.length; i++){
+//    if (userName.slice(i, i + 1) == "!"|| userName.slice(i, i + 1)=="@"||userName.slice(i , i +
+//1))
+//   alert("Invalid username");
+//}
+//
+////chapter no 21-25 task no 14
+//var arr = ['cake','apple pie','cookies','chips','patties']
+//var b = prompt('Enter bakery item');
+//var b = b.toLowerCase();
+//var bb = arr.indexOf(b);
+//if (arr.indexOf(b) != -1){
+//        alert( b + " is available in our bakery");
+//    }
+//    else{ 
+//       alert( b + " is not available in our bakery ");
+//    }
+
+////chapter no 21-25 task no 16
+//var a = "university of karachi"
+//var b = a.Split('')
+//for(var i = 0;i<b.length;i ++){
+//document.write(b[i], "<br>")
+//}
+///chapter no 21-25 task no 17
+//var text = 'Pakistan'
+//var indxNum = text.lastIndexOf('n');
+//var firstText = text.slice(0,indxNum)
+//document.write('Last character of  input:'+ n)
+
+//chapter no  task no 13
+//var user =prompt("enter your password ")
+//if(user === charCodeAt > 65 || < 122 && ! num){
+//alert("Enter password: 123cmad" )
+//}
+//else if( user === ! user === charCodeAt()){
+//alert("password can not begin with a number")
+//}
+//else if(user.valueOf.length >6{
+//alert("password.value.length ")
+//}
+//chapter no 31-34 task no 1 
+//var a = new Date();
+//document.write(a)
+
+//chapter no 31-34 task no 1
+//var d = new Date();
+//d.setFullYear(d.getFullYear(), d.getMonth() + 6,)
+//document.write("later date:" + d)
+
